4876d8d8b9a8d1c5db11b223ef5d8262ebb2ac70
Rodolfo Hansen <rhansen@kitsd.com>
2024-09-12 12:31:44 +0200

    More fleshed out example

--- src/test/scala/eo/Unthreaded.scala
+++ src/test/scala/eo/Unthreaded.scala
@@ -1,17 +1,38 @@
 package eo
 
 import optics.*
+import data.Forget
 
 import scodec.Codec
-import scodec.implicits.*
 import scodec.bits.BitVector
 
-case class Person(id: Int, name: String, phones: Array[Phone])
+import scala.collection.immutable.ArraySeq
+import cats.instances.arraySeq.*
+
+case class Person(id: Int, name: String, phones: ArraySeq[Phone])
 case class Phone(isMobile: Boolean, number: String)
 
 @main def unthreaded =
-  val personId = Lens[Person, Int](_.id, (s, b) => s.copy(b))
+  import eo.data.Forgetful.given
+
+  val personPhone =
+    Lens[Person, ArraySeq[Phone]](_.phones, (s, b) => s.copy(phones = b))
+      .morph[Forget[ArraySeq]]
+      .andThen[Phone, Phone](Traversal.each[ArraySeq, Phone])
+
   val phoneIsMobile = Lens[Phone, Boolean](_.isMobile, (s, b) => s.copy(b))
 
   val personCodec = Prism[Person, BitVector](Codec.encode.andthen(_.require), Codec.decode)
   val phoneCodec = Prism[Person, BitVector](Codec.encode.andthen(_.require), Codec.decode)
+
+  val personPipeline: BitVector => BitVector = ???
+  val phonePipeline: BitVector => BitVector = ???
+
+  val personPhonesAreMobile: Optic[Person, Person, Boolean, Boolean, Forget[ArraySeq]] = ???
+  val personPlaceIsMobile: Optic[Person, BitVector, Boolean, Boolean, Forget[ArraySeq]] = ???
+
+  val Bob = Person(1, "John", ArraySeq(Phone(true, "+310655940946")))
+  val Alice = Person(1, "John", ArraySeq(Phone(false, "+88 55940946")))
+
+  val BobBits: BitVector = personCodec.get(Bob)
+  val AliceBits: BitVector = personCodec.get(Bob)
